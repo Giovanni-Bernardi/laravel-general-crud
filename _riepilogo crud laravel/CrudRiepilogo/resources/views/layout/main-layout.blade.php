58401555a61fdf72f30a8f11c1965ecca2fe5f09
Giovanni <giovannibernardi13@gmail.com>
2021-06-03 12:37:49 +0200

    complete exercise

--- _riepilogo crud laravel/CrudRiepilogo/resources/views/layout/main-layout.blade.php
+++ _riepilogo crud laravel/CrudRiepilogo/resources/views/layout/main-layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>GIOVANNI'S LEAGUE</title>
+    <title>GIOVANNI'S League</title>
     {{-- ccs --}}
     <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
     {{-- google font --}}
